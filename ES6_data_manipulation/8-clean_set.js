069c2c1a3172e693849d6bfd8937c23f5992a7a5
stan-holberton <9857@holbertonstudents.com>
2025-04-02 15:55:29 +0200

    add 8-main.js 8-clean_set.js files for the task 8.

---
+++ ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,11 @@
+export default function cleanSet(set, startString) {
+    if (!startString || typeof startString !== 'string' || startString === '') {
+      return '';
+    }
+  
+    const filteredValues = [...set]
+      .filter((value) => typeof value === 'string' && value.startsWith(startString))
+      .map((value) => value.slice(startString.length));
+  
+    return filteredValues.join('-');
+  }
