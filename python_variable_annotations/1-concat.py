f9b2120f4767e70b544d89de7019860b85cfc94b
stan <9857@holbertonstudents.com>
2025-04-23 11:30:27 +0200

    add 1-concat file

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Module that provides a function to concatenate two strings"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Concatenate two strings.
+
+    Args:
+        str1 (str): First string
+        str2 (str): Second string
+
+    Returns:
+        str: The concatenation of str1 and str2
+    """
+    return str1 + str2
