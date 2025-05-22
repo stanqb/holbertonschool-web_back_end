236970463039e71fa4b05f64a3be877be0ffab82
stanqb <9857@holbertonstudents.com>
2025-05-23 00:43:11 +0200

    Fix ESLint errors and formatting

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -1,37 +1,29 @@
-// import module fs to read file
 const fs = require('fs');
+
 function countStudents(path) {
  let students = 0;
- // try to read the file
  try {
    const data = fs.readFileSync(path, 'utf-8');
-   // count number of non blank line
    const lines = data.split('\n');
    for (let counter = 0; counter < lines.length; counter += 1) {
      if (lines[counter].length > 0) students += 1;
    }
-   // skip header if db is not empty
    if (students > 0) {
      students -= 1;
    }
    console.log(`Number of students: ${students}`);
-   // empty array for classroom
    const classrooms = [];
-   // loop on db to find each classroom (except on first line (headers))
    lines.slice(1).forEach((line) => {
      const parts = line.split(',');
      const field = parts[3];
-     // if field and classromm exist; add to the array
      if (field && !classrooms.includes(field)) {
        classrooms.push(field);
      }
    });
-   // for each classroom, create a empty array
    const grouped = {};
    classrooms.forEach((field) => {
      grouped[field] = [];
    });
-   // loop again on db and add firstname to each classroom array
    lines.slice(1).forEach((line) => {
      const parts = line.split(',');
      const firstname = parts[0];
@@ -40,7 +32,6 @@ function countStudents(path) {
        grouped[field].push(firstname);
      }
    });
-   // show results
    classrooms.forEach((field) => {
      console.log(`Number of students in ${field}: ${grouped[field].length}. List: ${grouped[field].join(', ')}`);
    });
--- Node_JS_basic/3-read_file_async.js
+++ Node_JS_basic/3-read_file_async.js
@@ -1,40 +1,33 @@
-// import module fs to read file
 const fs = require('fs');
+
 function countStudents(path) {
  let students = 0;
  return new Promise((resolve, reject) => {
-   // try to read the file
    fs.readFile(path, 'utf-8', (err, data) => {
      if (err) {
-       return reject(new Error('Cannot load the database'));
+       reject(new Error('Cannot load the database'));
+       return;
      }
-     // count number of non blank line
      const lines = data.split('\n');
      for (let counter = 0; counter < lines.length; counter += 1) {
        if (lines[counter].length > 0) students += 1;
      }
-     // skip header if db is not empty
      if (students > 0) {
        students -= 1;
      }
      console.log(`Number of students: ${students}`);
-     // empty array for classroom
      const classrooms = [];
-     // loop on db to find each classroom (except on first line (headers))
      lines.slice(1).forEach((line) => {
        const parts = line.split(',');
        const field = parts[3];
-       // if field and classromm exist; add to the array
        if (field && !classrooms.includes(field)) {
          classrooms.push(field);
        }
      });
-     // for each classroom, create a empty array
      const grouped = {};
      classrooms.forEach((field) => {
        grouped[field] = [];
      });
-     // loop again on db and add firstname to each classroom array
      lines.slice(1).forEach((line) => {
        const parts = line.split(',');
        const firstname = parts[0];
@@ -43,11 +36,10 @@ function countStudents(path) {
          grouped[field].push(firstname);
        }
      });
-     // show results
      classrooms.forEach((field) => {
        console.log(`Number of students in ${field}: ${grouped[field].length}. List: ${grouped[field].join(', ')}`);
      });
-     return resolve();
+     resolve();
    });
  });
 }
