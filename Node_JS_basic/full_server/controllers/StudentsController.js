43d28d5ac3ccc4681cca3c94f24935e2955d1e3a
stanqb <9857@holbertonstudents.com>
2025-05-23 10:25:07 +0200

    fixe eslint error in full server directory for the task 8.

--- Node_JS_basic/full_server/controllers/StudentsController.js
+++ Node_JS_basic/full_server/controllers/StudentsController.js
@@ -3,16 +3,16 @@ const readDatabase = require('../utils');
 class StudentsController {
   static getAllStudents(request, response) {
     const databaseFile = process.argv[2];
-    
+
     readDatabase(databaseFile)
       .then((fields) => {
         let responseText = 'This is the list of our students';
         const sortedFields = Object.keys(fields).sort();
-        
+
         sortedFields.forEach((field) => {
           responseText += `\nNumber of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`;
         });
-        
+
         response.status(200).send(responseText);
       })
       .catch(() => {
@@ -23,12 +23,12 @@ class StudentsController {
   static getAllStudentsByMajor(request, response) {
     const databaseFile = process.argv[2];
     const { major } = request.params;
-    
+
     if (major !== 'CS' && major !== 'SWE') {
       response.status(500).send('Major parameter must be CS or SWE');
       return;
     }
-    
+
     readDatabase(databaseFile)
       .then((fields) => {
         const students = fields[major] || [];
