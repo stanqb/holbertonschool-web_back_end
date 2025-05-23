63a450852f90a7df9a2288bb94aafcfa1212d962
stanqb <9857@holbertonstudents.com>
2025-05-23 10:00:46 +0200

    fixe some eslint error to task 0, 1 , 2, 3, 4, 5 and 7.

--- Node_JS_basic/3-read_file_async.js
+++ Node_JS_basic/3-read_file_async.js
@@ -1,46 +1,46 @@
 const fs = require('fs');
 
 function countStudents(path) {
- let students = 0;
- return new Promise((resolve, reject) => {
-   fs.readFile(path, 'utf-8', (err, data) => {
-     if (err) {
-       reject(new Error('Cannot load the database'));
-       return;
-     }
-     const lines = data.split('\n');
-     for (let counter = 0; counter < lines.length; counter += 1) {
-       if (lines[counter].length > 0) students += 1;
-     }
-     if (students > 0) {
-       students -= 1;
-     }
-     console.log(`Number of students: ${students}`);
-     const classrooms = [];
-     lines.slice(1).forEach((line) => {
-       const parts = line.split(',');
-       const field = parts[3];
-       if (field && !classrooms.includes(field)) {
-         classrooms.push(field);
-       }
-     });
-     const grouped = {};
-     classrooms.forEach((field) => {
-       grouped[field] = [];
-     });
-     lines.slice(1).forEach((line) => {
-       const parts = line.split(',');
-       const firstname = parts[0];
-       const field = parts[3];
-       if (firstname && field) {
-         grouped[field].push(firstname);
-       }
-     });
-     classrooms.forEach((field) => {
-       console.log(`Number of students in ${field}: ${grouped[field].length}. List: ${grouped[field].join(', ')}`);
-     });
-     resolve();
-   });
- });
+  let students = 0;
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf-8', (err, data) => {
+      if (err) {
+        reject(new Error('Cannot load the database'));
+        return;
+      }
+      const lines = data.split('\n');
+      for (let counter = 0; counter < lines.length; counter += 1) {
+        if (lines[counter].length > 0) students += 1;
+      }
+      if (students > 0) {
+        students -= 1;
+      }
+      console.log(`Number of students: ${students}`);
+      const classrooms = [];
+      lines.slice(1).forEach((line) => {
+        const parts = line.split(',');
+        const field = parts[3];
+        if (field && !classrooms.includes(field)) {
+          classrooms.push(field);
+        }
+      });
+      const grouped = {};
+      classrooms.forEach((field) => {
+        grouped[field] = [];
+      });
+      lines.slice(1).forEach((line) => {
+        const parts = line.split(',');
+        const firstname = parts[0];
+        const field = parts[3];
+        if (firstname && field) {
+          grouped[field].push(firstname);
+        }
+      });
+      classrooms.forEach((field) => {
+        console.log(`Number of students in ${field}: ${grouped[field].length}. List: ${grouped[field].join(', ')}`);
+      });
+      resolve();
+    });
+  });
 }
 module.exports = countStudents;
