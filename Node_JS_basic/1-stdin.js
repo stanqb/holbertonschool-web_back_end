9d414eff177229cdce42a6b67b13a3534bea2873
stanqb <9857@holbertonstudents.com>
2025-05-23 09:33:09 +0200

    fix lint error task 1

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,6 +1,12 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
-process.stdin.on('data', (userInput) => {
- process.stdout.write(`Your name is: ${userInput}`);
- process.stdout.write('This important software is now closing\n');
- process.exit();
+
+process.stdin.on('readable', () => {
+  const chunk = process.stdin.read();
+  if (chunk !== null) {
+    process.stdout.write(`Your name is: ${chunk}`);
+  }
+});
+
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
 });
