ed53a736662223b77fd234f34c01ea1c101e1207
stanqb <9857@holbertonstudents.com>
2026-02-23 11:08:36 +0100

    Task 6: add is_valid function to validate passwords against bcrypt hashes

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -6,3 +6,8 @@ import bcrypt
 def hash_password(password: str) -> bytes:
     """Return a salted, hashed version of the given password."""
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """Return True if the password matches the hashed password."""
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
