37fba3ca40d19e5283cd364a2d58fe685d75c980
stanqb <9857@holbertonstudents.com>
2026-02-24 10:59:04 +0100

    fix pycodesstyle on basic_auth.py

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,7 +3,6 @@
 from api.v1.auth.auth import Auth
 from models.user import User
 import base64
-from typing import TypeVar
 
 
 class BasicAuth(Auth):
@@ -34,9 +33,7 @@ class BasicAuth(Auth):
             return None
 
     def extract_user_credentials(
-                    self, 
-                    decoded_base64_authorization_header: str
-            ) -> (str, str):  # type: ignore
+            self, decoded_base64_authorization_header: str) -> tuple[str, str]:
         """Return the user email and password from the decoded Base64 value."""
         if decoded_base64_authorization_header is None:
             return None, None
@@ -48,8 +45,7 @@ class BasicAuth(Auth):
         return email, password
 
     def user_object_from_credentials(
-                    self, user_email: str, user_pwd: str
-            ) -> TypeVar('User'):  # type: ignore
+            self, user_email: str, user_pwd: str) -> User:
         """Return the User instance based on email and password."""
         if user_email is None or not isinstance(user_email, str):
             return None
