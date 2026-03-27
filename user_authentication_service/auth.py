e806091f661eef7ef771a3c7648b495dd411f6f2
stanqb <9857@holbertonstudents.com>
2026-03-27 15:23:59 +0100

    Task 8: Add Auth.valid_login method
    - Implement valid_login using bcrypt.checkpw to verify the password
    - Return False if the user does not exist or the password does not match

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -28,8 +28,8 @@ class Auth:
             return self._db.add_user(email, _hash_password(password))
 
     def valid_login(self, email: str, password: str) -> bool:
-        """Validate user credentials and
-        return True if correct, False otherwise."""
+        """Validate user credentials and return True
+        if correct, False otherwise."""
         try:
             user = self._db.find_user_by(email=email)
             return bcrypt.checkpw(password.encode("utf-8"),
