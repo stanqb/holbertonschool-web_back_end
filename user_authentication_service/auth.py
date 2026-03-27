860a9ac10e3440710920489e7f7b50fd6138ee7b
stanqb <9857@holbertonstudents.com>
2026-03-27 15:19:12 +0100

    Task 5: Add Auth.register_user method
    - Implement register_user in Auth class
    - Raise ValueError if the email is already registered
    - Hash the password with _hash_password before saving to the database

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -1,9 +1,28 @@
 #!/usr/bin/env python3
 """Auth module providing authentication utilities."""
 import bcrypt
+from db import DB
+from user import User
+from sqlalchemy.orm.exc import NoResultFound
 
 
 def _hash_password(password: str) -> bytes:
-    """Hash a password string using bcrypt
-    and return the salted hash as bytes."""
+    """Hash a password string using bcrypt and
+    return the salted hash as bytes."""
     return bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
+
+
+class Auth:
+    """Auth class to interact with the authentication database."""
+
+    def __init__(self) -> None:
+        """Initialize a new Auth instance."""
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """Register a new user and return the User object."""
+        try:
+            self._db.find_user_by(email=email)
+            raise ValueError("User {} already exists".format(email))
+        except NoResultFound:
+            return self._db.add_user(email, _hash_password(password))
