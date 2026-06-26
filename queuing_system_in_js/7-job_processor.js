774f08591092546d23ce723164401b387d159426
stanqb <9857@holbertonstudents.com>
2026-06-26 14:22:33 +0200

    Task 9: Add Kue job processor with blacklist and progress tracking

---
+++ queuing_system_in_js/7-job_processor.js
@@ -0,0 +1,22 @@
+import kue from 'kue';
+
+const blacklistedNumbers = ['4153518780', '4153518781'];
+
+const queue = kue.createQueue();
+
+function sendNotification(phoneNumber, message, job, done) {
+  job.progress(0, 100);
+
+  if (blacklistedNumbers.includes(phoneNumber)) {
+    done(new Error(`Phone number ${phoneNumber} is blacklisted`));
+    return;
+  }
+
+  job.progress(50, 100);
+  console.log(`Sending notification to ${phoneNumber}, with message: ${message}`);
+  done();
+}
+
+queue.process('push_notification_code_2', 2, (job, done) => {
+  sendNotification(job.data.phoneNumber, job.data.message, job, done);
+});
