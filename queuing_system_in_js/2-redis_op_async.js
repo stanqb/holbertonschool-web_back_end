6059387eae56e5d77ff559e6543284d259d7218d
stanqb <9857@holbertonstudents.com>
2026-06-26 14:05:53 +0200

    Task 3: Use promisify and async/await for displaySchoolValue

---
+++ queuing_system_in_js/2-redis_op_async.js
@@ -0,0 +1,29 @@
+import { promisify } from 'util';
+import { createClient, print } from 'redis';
+
+const client = createClient();
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+client.on('error', (err) => {
+  console.log(`Redis client not connected to the server: ${err.message}`);
+});
+
+function setNewSchool(schoolName, value) {
+  client.set(schoolName, value, print);
+}
+
+const getAsync = promisify(client.get).bind(client);
+
+async function displaySchoolValue(schoolName) {
+  const reply = await getAsync(schoolName);
+  console.log(reply);
+}
+
+(async () => {
+  await displaySchoolValue('Holberton');
+  setNewSchool('HolbertonSanFrancisco', '100');
+  await displaySchoolValue('HolbertonSanFrancisco');
+})();
